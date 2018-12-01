3dd6afbec59c0f4e5d16c5bbd25d9ffcc1e1ab98
Michael J. Curry <curry@cs.umd.edu>
2018-12-01 12:18:08 -0500

    able to read and split string; hooray

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+mod util;
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("./puzzle_inputs/day11.txt")?;
+    let mut input_string = String::new();
+    let mut buf_reader = BufReader::new(file);
+    buf_reader.read_to_string(&mut input_string)?;
+    let split = util::split_and_parse_string(&input_string);
+
+    println!("{}", input_string);
+    Ok(())
 }
---
+++ src/util.rs
@@ -0,0 +1,30 @@
+use std::string::ParseError;
+pub fn split_and_parse_string(str: &String) -> Vec<i32> {
+    let split_vec: Vec<&str> = str.split("\n").collect();
+
+    let nums= split_vec.into_iter()
+        .flat_map(|s: &str| {s.parse()}).collect();
+
+    nums
+}
+
+#[cfg(test)]
+mod tests {
+    use super::split_and_parse_string;
+    #[test]
+    fn parse_works_empty() {
+        let st: String = String::from("test");
+        let results = split_and_parse_string(&st);
+        assert_eq!(results.len(), 0);
+    }
+
+    #[test]
+    fn parse_works_some() {
+        let st = String::from("1\n2\n3\n");
+        let results = split_and_parse_string(&st);
+        assert_eq!(results[0], 1);
+        assert_eq!(results[1], 2);
+        assert_eq!(results[2], 3);
+    }
+
+}
